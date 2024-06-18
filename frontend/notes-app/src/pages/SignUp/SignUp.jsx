d6de47877f14a6b2fa8af2d60c093396a0da5bb4
PhatBen234 <158719289+PhatBen234@users.noreply.github.com>
2024-06-18 16:15:30 +0700

    add_signup

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import PasswordInput from "../../components/Input/PasswordInput";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
 
 const SignUp = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
   const handleSignUp = async (e) => {
     e.preventDefault();
 
@@ -28,7 +32,41 @@ const SignUp = () => {
     }
 
     setError("");
+
+    try {
+      const response = await axiosInstance.post("/user/create-account", {
+        fullName: name,
+        email: email,
+        password: password,
+      });
+
+      // Log the response for debugging
+      console.log("API Response:", response);
+
+      if (response.data && response.data.error) {
+        setError(response.data.message);
+        return;
+      }
+
+      if (response.data && response.data.accessToken) {
+        localStorage.setItem("token", response.data.accessToken);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
+      } else {
+        setError("An unexpected error occurred");
+      }
+    }
   };
+
   return (
     <>
       <Navbar />
