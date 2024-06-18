e0e3d0156c67195fc4d753b86784b85690774ae1
PhatBen234 <158719289+PhatBen234@users.noreply.github.com>
2024-06-18 17:28:33 +0700

    get_notes

--- frontend/notes-app/src/components/Cards/NoteCard.jsx
+++ frontend/notes-app/src/components/Cards/NoteCard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import moment from "moment";
 import { MdOutlinePushPin, MdCreate, MdDelete } from "react-icons/md";
+
 const NoteCard = ({
   title,
   date,
@@ -15,16 +17,28 @@ const NoteCard = ({
       <div className="flex items-center justify-between">
         <div>
           <h6 className="text-sm font-medium">{title}</h6>
-          <span className="text-xs text-slate-500">{date}</span>
+          <span className="text-xs text-slate-500">
+            {moment(date).format("Do MMM YYYY")}
+          </span>
         </div>
 
-        <MdOutlinePushPin className={`icon-btn ${isPinned ? 'text-primary' : 'text-slate-300'}`} onClick={onPinNote} />
+        <MdOutlinePushPin
+          className={`icon-btn ${isPinned ? "text-primary" : "text-slate-300"}`}
+          onClick={onPinNote}
+        />
       </div>
 
       <p className="text-xs text-slate-600 mt-2">{content?.slice(0, 60)}</p>
 
       <div className="flex items-center justify-between mt-2">
-        <div className="text-xs text-slate-500">{tags}</div>
+        <div className="text-xs text-slate-500">
+          {tags.map((item, index) => (
+            <span key={index}>
+              #{item}
+              {index < tags.length - 1 && " "}
+            </span>
+          ))}
+        </div>
 
         <div className="flex items-center gap-2">
           <MdCreate
